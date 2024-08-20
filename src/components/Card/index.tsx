d13b06845b3c092835218b83f8f220656cc611c6
TimStt <timoshka200004@gmail.com>
2024-08-20 16:13:36 +0300

    fix

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -27,21 +27,3 @@ const Card: React.FC<IPropsDevice> = ({ device }) => {
 };
 
 export default Card;
-// type Repo = {
-//   name: string;
-//   stargazers_count: number;
-// };
-
-// export const getServerSideProps = (async (context) => {
-//   const res = await fetch("https://api.github.com/repos/vercel/next.js");
-//   const repo = await res.json();
-//   return { props: { repo } };
-// }) satisfies GetServerSideProps<{
-//   repo: Repo;
-// }>;
-
-// export default function Page({
-//   repo,
-// }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-//   return repo.stargazers_count;
-// }
